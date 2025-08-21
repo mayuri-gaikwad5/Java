605026bf6023c2184eaa91983dacdcc71dda6e22
Mayuri Gaikwad <2305mayurigaikwad@gmail.com>
2025-08-21 15:26:50 +0530

    Question1

---
+++ Lab_1/Introduction.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+public class Introduction
+{
+    public static void main(String args[])
+    {
+        LocalDate currentDateTime=LocalDate.now();
+        String name = "Mayuri Gaikwad";
+        String college= "WIT , Solapur";
+        System.out.println("Name : "+name);
+        System.out.println("College :"+college);
+        System.out.println("Current Date :"+currentDateTime);
+        
+    }
+}
